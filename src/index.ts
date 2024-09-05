6f6329674364fd2983759c7b0a620adbc2e4a02e
Maxim Yakovlev <it.trainee@yandex.ru>
2024-09-05 16:11:53 +0300

    refactoring: index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+export * from './utils/events/EventEmitter';
+export * from './utils/promisify/Promisify';
+
 export * from './shapes/circle/Circle';
 export * from './shapes/rectangle/Rectangle';
 export * from './shapes/triangle/Triangle';
